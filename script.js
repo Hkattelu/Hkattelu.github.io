1f692b76c5f1b044c1f1a83547e4e8cb4c765d62
Himanshu_Kattelu <Himanshukattelu@gmail.com>
2016-05-11 14:17:33 -0400

    big site update

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
 $(document).ready(function() {
-	
-
-  $("#description2").hide();
-  $("#picture2").hide();
-
-  $("#work").click(function(){
-     
-     $("#work").addClass('active');
-     $('#other').removeClass('active');
-     $('#description2').hide();
-     $('#description1').show();
-     $("#picture2").hide();
-     $("#picture1").show();
-
-  });
-
-  $("#other").click(function(){
-     
-     $("#other").addClass('active');
-     $('#work').removeClass('active');
-     $('#description1').hide();
-     $('#description2').show();
-     $("#picture1").hide();
-     $("#picture2").show();
-
-
-  });  
-
-  
-
 
 });
