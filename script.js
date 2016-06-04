827df9461ff96f42b7182640dc7fb4dff7a2bfc2
Himanshu_Kattelu <Himanshukattelu@gmail.com>
2016-06-04 12:16:18 -0400

    Overhauled site again

--- script.js
+++ script.js
@@ -1,3 +1,113 @@
+var sections = ["home","skills","projects"];
+var active = sections[0];
+
 $(document).ready(function() {
 
+	$("#" + sections[1]).hide();
+	$("#" + sections[2]).hide();
+
+	$("#buttonHome").click(function(){
+		setTimeout(function(){
+			document.getElementsByClassName("card")[0].style.transition="all 0.6s ease";
+			document.getElementsByClassName("card")[0].style.transform="rotateX(360deg)";
+			setTimeout(function(){
+				document.getElementById(active).style.visibility="hidden";
+				document.getElementsByClassName("card")[0].style.transition="all 0.6s ease";
+				document.getElementsByClassName("card")[0].style.transform="rotateX(0deg)";
+				setTimeout(function(){
+					$("#" + sections[1]).hide();
+					$("#" + sections[2]).hide();
+					document.getElementById("home").style.visibility="visible";
+					active = sections[0];
+					$("#" + sections[0]).show();
+				},600);
+			},600);
+		},100);
+	});
+	$("#buttonSkills").click(function(){
+		setTimeout(function(){
+			document.getElementsByClassName("card")[0].style.transition="all 0.6s ease";
+			document.getElementsByClassName("card")[0].style.transform="rotateX(360deg)";
+			setTimeout(function(){
+				document.getElementById(active).style.visibility="hidden";
+				document.getElementsByClassName("card")[0].style.transition="all 0.6s ease";
+				document.getElementsByClassName("card")[0].style.transform="rotateX(0deg)";
+				setTimeout(function(){
+					$("#" + sections[0]).hide();
+					$("#" + sections[2]).hide();
+					document.getElementById("skills").style.visibility="visible";
+					active = sections[1];
+					$("#" + sections[1]).show();
+				},600);
+			},600);
+		},100);
+	});
+	$("#buttonProjects").click(function(){
+		setTimeout(function(){
+			document.getElementsByClassName("card")[0].style.transition="all 0.6s ease";
+			document.getElementsByClassName("card")[0].style.transform="rotateX(360deg)";
+			setTimeout(function(){
+				document.getElementById(active).style.visibility="hidden";
+				document.getElementsByClassName("card")[0].style.transition="all 0.6s ease";
+				document.getElementsByClassName("card")[0].style.transform="rotateX(0deg)";
+				setTimeout(function(){
+					$("#" + sections[0]).hide();
+					$("#" + sections[1]).hide();
+					document.getElementById("projects").style.visibility="visible";
+					active = sections[2];
+					$("#" + sections[2]).show();
+				},600);
+			},600);
+		},100);
+	});
+
+
+
+	// $("#buttonHome").click(function(){
+	
+	// 	$(active).hide(10,function(){
+	// 		active = sections[0];
+	// 		$(".card").animate({transform: "rotateX(180deg)",transition: "all 0.6s ease"}, 600,function(){
+
+	// 			$(".card").animate({transform: "rotateX(-180deg)",transition: "all 0.6s ease"}, 600, function(){
+
+	// 				$(active).show();
+				
+	// 			})
+	// 		})
+	// 	})
+	// });
+
+	// $("#buttonSkills").click(function(){
+	 
+	//  	$(active).hide(10,function(){
+	// 		active = sections[1];
+	// 		$(".card").animate({transform: "rotateX(180deg)",transition: "all 0.6s ease"}, 600,function(){
+
+	// 			$(".card").animate({transform: "rotateX(-180deg)",transition: "all 0.6s ease"}, 600, function(){
+
+	// 				$(active).show();
+				
+	// 			})
+	// 		})
+	// 	})
+	// });
+
+	// $("#buttonProjects").click(function(){
+	 
+	// 	$(active).hide(10,function(){
+	// 		active = sections[2];
+	// 		$(".card").animate({transform: "rotateX(180deg)",transition: "all 0.6s ease"}, 600,function(){
+
+	// 			$(".card").animate({transform: "rotateX(-180deg)",transition: "all 0.6s ease"}, 600, function(){
+
+	// 				$(active).show();
+				
+	// 			})
+	// 		})
+	// 	})
+	// });
+
+
 });
+
