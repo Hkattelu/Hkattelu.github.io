8be58e1941e10c742c3ecafdf928398e37dd4657
Himanshu Kattelu <himanshukattelu@gmail.com>
2024-12-17 23:19:06 -0500

    Clean up some stuff and try again

--- script.js
+++ script.js
@@ -1,101 +1,112 @@
 const pageMap = {
-    "-1": "home",
-    "0": "about-me",
-    "1": "what-i-value",
-    "2": "projects",
-    "3": "contact",
-    "4": "blog",
-    "5": "credits",
+  "-1": "home",
+  "0": "about-me",
+  "1": "what-i-value",
+  "2": "projects",
+  "3": "contact",
+  "4": "blog",
+  "5": "credits",
 }
 
 function getOptions() {
-    return document.querySelectorAll(".option");
+  return document.querySelectorAll(".option");
 }
 
 function getCurrentPage() {
-  return document.querySelector('.visible');
+return document.querySelector('.visible');
 }
 
 function hideOptions() {
-    const options = getOptions();
-    options.forEach((box) => {
-        box.style['transition-delay'] = "0s";
-        box.style.transform = "rotate3d(0, 0, 1, 90deg)";
-    });
+  const options = getOptions();
+  options.forEach((box) => {
+      box.style['transition-delay'] = "0s";
+      box.style.transform = "rotate3d(0, 0, 1, 90deg)";
+  });
 
-    const home = document.querySelector(".home");
-    home.classList.remove('visible');
+  const home = document.querySelector(".home");
+  home.classList.remove('visible');
 }
 
 function showOptions() {
-    const options = getOptions();
-    options.forEach((box) => {
-        box.style['transition-delay'] = "var(--base-transition-duration)";
-        box.style.transform = '';
-    });
+  const options = getOptions();
+  options.forEach((box) => {
+      box.style['transition-delay'] = "var(--base-transition-duration)";
+      box.style.transform = '';
+  });
 }
 
 function showPage(index) {
-    const page = document.getElementsByClassName(String(pageMap[index]))[0];
-    page.style['transition-delay'] = "var(--base-transition-duration)";
-    page.classList.add('visible');
+  const page = document.getElementsByClassName(String(pageMap[index]))[0];
+  page.style['transition-delay'] = "var(--base-transition-duration)";
+  page.classList.add('visible');
 }
 
 function backToHome() {
-    const pages = document.getElementsByClassName('page');
-    for (const page of pages) {
-      page.classList.remove('visible');
-    }
-    showPage(-1);
+  const pages = document.getElementsByClassName('page');
+  for (const page of pages) {
+    page.classList.remove('visible');
+  }
+  showPage(-1);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const options = getOptions();
-  
-    options.forEach((box, index) => {
-      box.addEventListener('click', function(event) {
-        hideOptions();
-        showPage(index);
-      });
+  const options = getOptions();
 
-      box.addEventListener('keypress', function(event) {
-        hideOptions();
-        showPage(index);
-      });
+  options.forEach((box, index) => {
+    box.addEventListener('click', function(event) {
+      hideOptions();
+      showPage(index);
     });
 
-    setupGlider();
+    box.addEventListener('keydown', function(event) {
+      if (event.key !== 'Enter') {
+        return;
+      }
+      hideOptions();
+      showPage(index);
+      event.preventDefault();
+    });
+  });
+
+  setupGlider();
 });
 
 let glide;
 function setupGlider() {
-    glide = new Glide('.glide', {
-        startAt: 0,
-        dots: '#dots',
-        draggable: true,
-        arrows: {
-            prev: '.glider-prev',
-            next: '.glider-next'
-        }
-    });
-    glide.mount();
+  glide = new Glide('.glide', {
+      startAt: 0,
+      dots: '#dots',
+      draggable: true,
+      arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+      }
+  });
+  glide.mount();
 }
 
 document.addEventListener('keydown', function(event) {
-    if (event.key === 'Backspace' || event.key === 'Escape') {
+  switch (event.code) {
+    case 'Backspace':
+    case 'Escape':
       showOptions();
       backToHome();
-    }
-    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+      event.preventDefault();
+      break;
+    case 'ArrowUp':
+    case 'ArrowDown':
       if (document.activeElement.tagName.toLowerCase() !== 'span') {
         document.querySelector('span:not(:focus)').focus();
         event.preventDefault();
       } else if (!getCurrentPage().classList.contains('home')) {
         event.preventDefault();
       }
-    }
-    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
-        glide.go(event.key === 'ArrowLeft' ? '<' : '>');
-        event.preventDefault();
-    }
-  });
+      break;
+    case 'ArrowLeft':
+    case 'ArrowRight':
+      glide.go(event.code === 'ArrowLeft' ? '<' : '>');
+      event.preventDefault();
+    default:
+      break;
+  }
+});
